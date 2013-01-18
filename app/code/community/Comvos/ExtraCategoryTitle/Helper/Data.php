f06b75ec8143c001ff8bab41e0e1d88f80d39e20
root <root@curie.comvos.de>
2013-01-18 17:38:48 +0100

    I have forgotten a comvos directory in app/comunity

---
+++ app/code/community/Comvos/ExtraCategoryTitle/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: senders
+ * Date: 27.11.12
+ * Time: 11:50
+ * To change this template use File | Settings | File Templates.
+ */ 
+class Comvos_ExtraCategoryTitle_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
